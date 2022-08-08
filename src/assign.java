4693bff562b4d2899f97a4b195b5ee77851c8906
Chenchu Sai Kumar <SaiKumar.Chenchu@ivycomptech.com>
2022-08-08 16:48:53 +0530

    message

--- src/assign.java
+++ src/assign.java
@@ -1,44 +1,36 @@
-import java.util.*;
-class Second {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a[] = new int[7];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = sc.nextInt();
-        }
-        float tot = 0.0f;
-        int t = 0;
-        for (int i = 0; i < a.length; i++) {
-            t += a[i];
-            tot = tot + a[i] * 100;
-            if (a[i] > 8) {
-                int temp = a[i] - 8;
-                tot = tot + (temp * 15);
+import java.util.Scanner;
+
+    public class DemoTranslation {
+        public static void main(String[] args) {
+            int x, y, z;
+            String8 lab = new String8(2);
+            System.out.println("Enter x");
+            x = STDIN_SCANNER.nextInt();
+            System.out.println("Enter y");
+            y = STDIN_SCANNER.nextInt();
+            System.out.println("Enter z");
+            z = STDIN_SCANNER.nextInt();
+            System.out.println("Enter the lab allocated for ACE training");
+            lab.copyFrom(STDIN_SCANNER.next());
+            if(lab.equals("L1")) {
+                if(y < z) {
+                    System.out.print("L2 has the minimal seating capacity");
+                } else {
+                    System.out.print("L3 has the minimal seating capacity");
+                }
+            } else if(lab.equals("L2")) {
+                if(x < z) {
+                    System.out.print("L1 has the minimal seating capacity");
+                } else {
+                    System.out.print("L3 has the minimal seating capacity");
+                }
+            } else if(x < y) {
+                System.out.print("L1 has the minimal seating capacity");
+            } else {
+                System.out.print("L2 has the minimal seating capacity");
             }
         }
-        if (t > 40) {
-            int temp1 = 40 - t;
-            tot = tot + temp1 * 25;
-        }
-        float sun_d = 0;
-        if (a[0] > 0) {
-            sun_d = a[0] * 100;
-            if (a[0] > 8) {
-                int sun1 = a[0] - 8;
-                sun_d = sun_d + (sun1 * 15);
-            }
-            sun_d = sun_d + (sun_d * 0.50f);
-        }
-        float sat_d = 0;
-        if (a[6] > 0) {
-            sat_d = a[6] * 100;
-            if (a[6] > 8) {
-                int sat1 = a[6] - 8;
-                sat_d = sat_d + (sat1 * 15);
-            }
-            sat_d = sat_d + (sat_d * 0.25f);
-        }
-        tot = tot + sun_d + sat_d;
-        System.out.print(tot);
+
+        public final static Scanner STDIN_SCANNER = new Scanner(System.in);
     }
-}
+
